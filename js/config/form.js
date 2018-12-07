ddd6bf7f1bbd66b065332c4a7ab999d2c0f08cc3
Brian Smith <briaguya@gmail.com>
2018-12-07 14:32:29 -0600

    added about you page

--- js/config/form.js
+++ js/config/form.js
@@ -380,9 +380,90 @@ const formConfig = {
         }
       }
     },
-    secondChapter: {
-      title: "Second Chapter",
-      pages: {}
+    aboutYouChapter: {
+      pages: {
+        aboutYou: {
+          path: "about-you",
+          title: "Tell us about you",
+          schema: {
+            type: "object",
+            properties: {
+              "view:textObject": {
+                type: "object",
+                properties: {}
+              },
+              race: {
+                type: "string",
+                enum: [
+                  "white",
+                  "latino",
+                  "black",
+                  "asian",
+                  "indian",
+                  "arab",
+                  "native",
+                  "hawaiian",
+                  "other",
+                  "preferNot"
+                ],
+                enumNames: [
+                  "White or Euro-American",
+                  "Latino or Hispanic American",
+                  "Black, Afro-Caribbean, or African American",
+                  "East Asian or Asian American",
+                  "South Asian or Indian American",
+                  "Middle Eastern or Arab American",
+                  "Native American or Alaskan Native",
+                  "Native Hawaiian or Other Pacific Islander",
+                  "Other",
+                  "Prefer not to say"
+                ]
+              },
+              gender: {
+                type: "string",
+                enum: ["male", "female", "nonBinary", "preferNot"],
+                enumNames: ["Male", "Female", "Non-binary", "Prefer not to say"]
+              },
+              zipCode: { type: "string" },
+              "view:contactPreferences": {
+                type: "object",
+                properties: {
+                  willingToBeContacted: { type: "boolean" },
+                  wouldLikeToSpeakDirectly: { type: "boolean" }
+                }
+              }
+            }
+          },
+          uiSchema: {
+            "view:textObject": {
+              "ui:description": () => (
+                <div>
+                  <h2>Demographic information</h2>
+                  <p>
+                    This information helps us recognize police behavior trends
+                    across the City that lead to policy, cultural, and training
+                    changes.
+                  </p>
+                </div>
+              )
+            },
+            race: { "ui:title": "Your race", "ui:widget": "radio" },
+            gender: { "ui:title": "Your gender", "ui:widget": "radio" },
+            zipCode: { "ui:title": "Your zip code" },
+            "view:contactPreferences": {
+              "ui:title": "Contact Preferences",
+              willingToBeContacted: {
+                "ui:title":
+                  "I am willing to be contacted by the Civilian Office of Police Oversight and Accountability"
+              },
+              wouldLikeToSpeakDirectly: {
+                "ui:title":
+                  "I would like to speak to a Police Department supervisor directly to discuss my complaint."
+              }
+            }
+          }
+        }
+      }
     }
   }
 };
