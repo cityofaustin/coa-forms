33b45a6b1f35faf4df1021f89bf05725aa70eac8
Brian Smith <briaguya@gmail.com>
2018-12-19 02:05:27 -0600

    import some and go straight to the complaint

--- js/components/Introduction.jsx
+++ js/components/Introduction.jsx
@@ -18,7 +18,7 @@ class Introduction extends React.Component {
   render() {
     return (
       <div className="schemaform-intro">
-        <FormTitle title="Police Oversight and Accountability Form" />
+        <FormTitle title="Officer Complaint Form" />
         <h1>Tell us about your experience with the Austin Police Department</h1>
         <p>
           We are the Civilian Office of Police Oversight and Accountability. We
@@ -35,9 +35,28 @@ class Introduction extends React.Component {
           this process so that you are as comfortable as possible. Find out more
           about who we are and what we do.
         </p>
+        <h1>Here’s what to expect</h1>
+        <h2>Process</h2>
+        <p>
+          Your complaint will be investigated by Internal Affairs at the Austin
+          Police Department and overseen by the Civilian Office of Police
+          Oversight and Accountability.
+        </p>
+        <h2>Timeframe</h2>
+        <p>
+          Investigations could take as little as 30 days or up to six months to
+          complete. It may require 1-4 hours of your time over the course of
+          those months.
+        </p>
+        <h2>Outcomes</h2>
+        <p>
+          The nature of your case will determine if any policy change or
+          punishment of the officer(s) occurs. Find out more on potential
+          outcomes here.
+        </p>
         <ProgressButton
           onButtonClick={this.startForm}
-          buttonText="Fill out the online form"
+          buttonText="I agree, begin form"
           buttonClass="usa-button-primary schemaform-start-button"
           afterText="»"
         />
