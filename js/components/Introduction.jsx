fb645a830f5b6383e297b3b65b6fbd3b3ee8ec2d
easherma <ericandrewsherman@gmail.com>
2019-01-23 17:20:56 -0600

    First pass

--- js/components/Introduction.jsx
+++ js/components/Introduction.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import ProgressButton from "us-forms-system/lib/js/components/ProgressButton";
-import FormTitle from "us-forms-system/lib/js/components/FormTitle";
+import ProgressButton from 'us-forms-system/lib/js/components/ProgressButton';
+import FormTitle from 'us-forms-system/lib/js/components/FormTitle';
+import uswds from 'uswds';
 
 class Introduction extends React.Component {
   constructor(props) {
@@ -19,47 +20,101 @@ class Introduction extends React.Component {
     return (
       <div className="schemaform-intro">
         <FormTitle title="Officer Complaint Form" />
-        <h1>Tell us about your experience with the Austin Police Department</h1>
+
+        <ul className="usa-accordion-bordered">
+          <li>
+            <button
+              className="usa-accordion-button"
+              aria-expanded="true"
+              aria-controls="b-a1"
+            >
+              First Amendment
+            </button>
+            <div
+              id="b-a1"
+              className="usa-accordion-content"
+              aria-hidden="false"
+            >
+              <h4>Required information</h4>
+              <ul>
+                <li>What happened</li>
+                <li>Time </li>
+              </ul>
+              <hr />
+              <h4>Optional information</h4>
+              <ul>
+                <li>Officer(s) involved</li>
+                <li>Witness(es) </li>
+                <li>
+                  Your contact information for us to follow up with you
+                  (translators available)
+                </li>
+              </ul>
+              <ProgressButton
+                onButtonClick={this.startForm}
+                buttonText="Start"
+                buttonClass="usa-button-primary schemaform-start-button"
+              />
+            </div>
+          </li>
+          <li>
+            <button
+              className="usa-accordion-button"
+              aria-expanded="false"
+              aria-controls="b-a2"
+            >
+              Over the phone
+            </button>
+            <div id="b-a2" className="usa-accordion-content" aria-hidden="true">
+              Call the Office of Police Oversight at 512-972-2676. We’d be happy
+              to speak with you. If you need language translation, you can call
+              with a friend to translate for you or ask for a translator. Just
+              tell us the language you prefer. Office hours are Monday through
+              Friday, 9 am to 5 pm.
+            </div>
+          </li>
+          <li>
+            <button
+              className="usa-accordion-button"
+              aria-expanded="false"
+              aria-controls="b-a3"
+            >
+              In person
+            </button>
+            <div id="b-a3" className="usa-accordion-content" aria-hidden="true">
+              Call the Office of Police Oversight at 512-972-2676. We’d be happy
+              to speak with you. If you need language translation, you can call
+              with a friend to translate for you or ask for a translator. Just
+              tell us the language you prefer. Office hours are Monday through
+              Friday, 9 am to 5 pm.
+            </div>
+          </li>
+          <li>
+            <button
+              className="usa-accordion-button"
+              aria-expanded="false"
+              aria-controls="b-a4"
+            >
+              By mail
+            </button>
+            <div id="b-a4" className="usa-accordion-content" aria-hidden="true">
+              You can complete the form in person at the Office of Police
+              Oversight at 1520 Rutherford Lane, Austin, TX 78754. We are in
+              Building 1, on the 2nd floor, Suite 211. Visitor parking is in
+              front of the main entrance. You will need to check in with
+              building security at the front desk. If you need language
+              translation, you can bring a friend to translate for you or ask
+              for a translator. Just tell us the language you prefer. Office
+              hours are Monday through Friday, 9 am to 5 pm.
+            </div>
+          </li>
+        </ul>
+        <h3>About Us</h3>
         <p>
-          We are the Civilian Office of Police Oversight and Accountability. We
-          are an independent and unbiased office. In other words, we are not a
-          part of the Police Department.
+          We are the Office of Police Oversight, impartial and separate from the
+          Austin Police Department.
         </p>
-        <p>
-          By telling us about your experience, you help us hold officers
-          accountable and advise on policing best practices. We understand that
-          fear of police retaliation but we’d like to assure you
-        </p>
-        <p>
-          Police are not allowed to retaliate against you. We will facilitate
-          this process so that you are as comfortable as possible. Find out more
-          about who we are and what we do.
-        </p>
-        <h1>Here’s what to expect</h1>
-        <h2>Process</h2>
-        <p>
-          Your complaint will be investigated by Internal Affairs at the Austin
-          Police Department and overseen by the Civilian Office of Police
-          Oversight and Accountability.
-        </p>
-        <h2>Timeframe</h2>
-        <p>
-          Investigations could take as little as 30 days or up to six months to
-          complete. It may require 1-4 hours of your time over the course of
-          those months.
-        </p>
-        <h2>Outcomes</h2>
-        <p>
-          The nature of your case will determine if any policy change or
-          punishment of the officer(s) occurs. Find out more on potential
-          outcomes here.
-        </p>
-        <ProgressButton
-          onButtonClick={this.startForm}
-          buttonText="I agree, begin form"
-          buttonClass="usa-button-primary schemaform-start-button"
-          afterText="»"
-        />
+        <a href="">Learn more about who we are and what we do.</a>
       </div>
     );
   }
@@ -67,7 +122,7 @@ class Introduction extends React.Component {
 
 Introduction.propTypes = {
   route: PropTypes.object,
-  router: PropTypes.object
+  router: PropTypes.object,
 };
 
 export default Introduction;
