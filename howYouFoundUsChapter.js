25f7f8525417d8a03d468ce3299f34274af4048a
Brian Smith <briaguya@gmail.com>
2019-01-24 22:22:16 -0600

    why doesn't heroku like me?

--- howYouFoundUsChapter.js
+++ howYouFoundUsChapter.js
@@ -1,54 +1,54 @@
 const howYouFoundUsChapter = {
-  title: "Tell us how you found us",
+  title: 'Tell us how you found us',
   pages: {
     howYouFoundUs: {
-      path: "how-you-found-us",
-      title: "Tell us how you found us",
+      path: 'how-you-found-us',
+      title: 'Tell us how you found us',
       schema: {
-        type: "object",
+        type: 'object',
         properties: {
           howDidYouGetHere: {
-            type: "string",
+            type: 'string',
             enum: [
-              "search",
-              "officer",
-              "austindotgov",
-              "apd",
-              "communityorg",
-              "other"
+              'search',
+              'officer',
+              'austindotgov',
+              'apd',
+              'communityorg',
+              'other',
             ],
             enumNames: [
-              "Search engine (Google, Bing, etc.)",
-              "The officer",
-              "Austin.gov",
-              "Email, text, or paper material from Austin Police Department",
-              "Community Organization",
-              "Other"
-            ]
+              'Search engine (Google, Bing, etc.)',
+              'The officer',
+              'Austin.gov',
+              'Email, text, or paper material from Austin Police Department',
+              'Community Organization',
+              'Other',
+            ],
           },
-          howDidYouGetHereOther: { type: "string", "ui:collapsed": true }
-        }
+          howDidYouGetHereOther: { type: 'string', 'ui:collapsed': true },
+        },
       },
       uiSchema: {
-        "ui:title": "Tell us how you found us",
+        'ui:title': 'Tell us how you found us',
         howDidYouGetHere: {
-          "ui:title": "How did you get to this form?",
-          "ui:widget": "radio",
-          "ui:options": {
-            hideOnReviewIfFalse: true
-          }
+          'ui:title': 'How did you get to this form?',
+          'ui:widget': 'radio',
+          'ui:options': {
+            hideOnReviewIfFalse: true,
+          },
         },
         howDidYouGetHereOther: {
-          "ui:title": " ",
-          "ui:options": {
-            expandUnder: "howDidYouGetHere",
-            expandUnderCondition: "other",
-            hideOnReviewIfFalse: true
-          }
-        }
-      }
-    }
-  }
+          'ui:title': ' ',
+          'ui:options': {
+            expandUnder: 'howDidYouGetHere',
+            expandUnderCondition: 'other',
+            hideOnReviewIfFalse: true,
+          },
+        },
+      },
+    },
+  },
 };
 
 export default howYouFoundUsChapter;
