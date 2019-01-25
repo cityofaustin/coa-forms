b88417ce8d246d9a2dd3938cbd8bfdf7db5d7abf
Brian Smith <briaguya@gmail.com>
2019-01-24 20:20:46 -0600

    prettier a file to see what happens

--- aboutYouChapter.js
+++ aboutYouChapter.js
@@ -1,64 +1,65 @@
-import React from "react";
-import { raceBlocks, genderBlocks } from "@cityofaustin/usfs-schema-blocks";
+import React from 'react';
+import { raceBlocks, genderBlocks } from '@cityofaustin/usfs-schema-blocks';
 
 const aboutYouChapter = {
-  title: "Tell us about you",
+  title: 'Tell us about you',
   pages: {
     aboutYou: {
-      path: "about-you",
-      title: "Tell us about you",
+      path: 'about-you',
+      title: 'Tell us about you',
       schema: {
-        type: "object",
+        type: 'object',
         properties: {
-          "view:textObject": {
-            type: "object",
-            properties: {}
+          'view:textObject': {
+            type: 'object',
+            properties: {},
           },
           ...genderBlocks.schema,
           ...raceBlocks.schema,
-          zipCode: { type: "string" },
-          "view:contactPreferences": {
-            type: "object",
+          zipCode: { type: 'string' },
+          'view:contactPreferences': {
+            type: 'object',
             properties: {
-              willingToBeContacted: { type: "boolean" }
-            }
-          }
-        }
+              willingToBeContacted: { type: 'boolean' },
+            },
+          },
+        },
       },
       uiSchema: {
-        "ui:title": "Tell us about you",
-        "view:textObject": {
-          "ui:description": () => (
+        'ui:title': 'Tell us about you',
+        'view:textObject': {
+          'ui:description': () => (
             <div>
               <h2>Demographic information</h2>
               <p>
-This information helps us recognize trends across the City that lead to policy and training recommendations.
+                This information helps us recognize trends across the City that
+                lead to policy and training recommendations.
               </p>
             </div>
-          )
+          ),
         },
         ...genderBlocks.ui,
         ...raceBlocks.ui,
 
         zipCode: {
-          "ui:title": "Your zip code",
-          "ui:options": {
-            hideOnReviewIfFalse: true
-          }
+          'ui:title': 'Your zip code',
+          'ui:options': {
+            hideOnReviewIfFalse: true,
+          },
         },
-        "view:contactPreferences": {
-          "ui:title": "Contact Preferences",
+        'view:contactPreferences': {
+          'ui:title': 'Contact Preferences',
           willingToBeContacted: {
-            "ui:title":
-              "I am willing to be contacted by the Office of Police Oversight in two to four business days.",
-            "ui:options": {
-              hideOnReviewIfFalse: true
-            }
-          }
-        }
-      }
-    }
-  }
+            'ui:title':
+              'I am willing to be contacted by the Office of Police Oversight in two to four business days.',
+            'ui:options': {
+              hideOnReviewIfFalse: true,
+            },
+          },
+        },
+      },
+    },
+  },
 };
 
 export default aboutYouChapter;
