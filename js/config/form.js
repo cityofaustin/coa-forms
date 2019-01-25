561cc0617775b93eb53d0872e3cd1b4844280c9a
Brian Smith <briaguya@gmail.com>
2019-01-24 19:46:35 -0600

    try using the new chapters

--- js/config/form.js
+++ js/config/form.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Introduction from "../components/Introduction.jsx";
-import Confirmation from "../components/Confirmation.jsx";
+import React from 'react';
+import Introduction from '../components/Introduction.jsx';
+import Confirmation from '../components/Confirmation.jsx';
 
 import {
   whatHappenedChapter,
@@ -8,17 +8,17 @@ import {
   aboutYouChapter,
   howYouFoundUsChapter,
   shareEvidenceChapter,
-  witnessDetailsChapter
-} from "@cityofaustin/officer-form-chapters";
+  witnessDetailsChapter,
+} from './chapters';
 
 const formConfig = {
-  title: "File a complaint",
-  subTitle: "",
-  formId: "",
-  urlPrefix: "/",
-  trackingPrefix: "form-",
-  transformForSubmit: "",
-  submitUrl: "",
+  title: 'File a complaint',
+  subTitle: '',
+  formId: '',
+  urlPrefix: '/',
+  trackingPrefix: 'form-',
+  transformForSubmit: '',
+  submitUrl: '',
   introduction: Introduction,
   confirmation: Confirmation,
   defaultDefinitions: {},
@@ -30,8 +30,8 @@ const formConfig = {
     officerDetailsChapter,
     witnessDetailsChapter,
     aboutYouChapter,
-    howYouFoundUsChapter
-  }
+    howYouFoundUsChapter,
+  },
 };
 
 export default formConfig;
