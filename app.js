f8669b8241567125a5b16fbe4e805c72cb9c917a
Sergio Garcia <sergiogcx@gmail.com>
2019-02-25 21:07:59 -0600

    Updated routes & route translations

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ ReactDOM.render(
     <header className="schemaform-block-header site-header" role="banner">
       <div className="usa-nav-container form-nav">
         <nav className="language-nav">
-          <a className="active language" href="/police-oversight/complaint">
+          <a className="active language" href="/police-complain">
             English
           </a>
-          <a href="/fiscalizacion-de-policia/queja" className="second">
+          <a href="/policia-queja" className="second">
             Español
           </a>
         </nav>
