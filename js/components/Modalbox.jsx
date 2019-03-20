30e96c490ba9c292bb0d2eb8c88d95e5b3464835
Sergio <sergiogcx@gmail.com>
2019-03-20 12:07:05 -0500

    Style update

--- js/components/Modalbox.jsx
+++ js/components/Modalbox.jsx
@@ -10,6 +10,7 @@ class Modalbox extends React.Component {
 
         this.state = {
             title: "Warning",
+            language: "",
             link: props.link || "#"
         };
     }
@@ -22,22 +23,26 @@ class Modalbox extends React.Component {
         return(
             <div className="opo-modalbox">
                 <div className="opo-modalbox__container">
-                    <div className="opo-modalbox__title">Warning</div>
-                    <div className="opo-modalbox__crossbutton" onClick={() => this.props.hideModal()}>x</div>
-                    <div className="opo-modalbox__separator"></div>
-                    <div className="opo-modalbox__message">
-                        <p>If you leave, your data will be lost.</p>
-                        <p>Click <b>cancel</b> to close this message and return to the form.</p>
-                        <p>Click <b>I want to leave</b> if you wish to exit this form.</p>
+                    <div className="opo-modalbox__header">
+                        <div className="opo-modalbox__title">Start over in Spanish?</div>
+                        <div className="opo-modalbox__crossbutton" onClick={() => this.props.hideModal()}></div>
                     </div>
-                    <div className="opo-modalbox__buttons">
-                        <button onClick={() => this.props.hideModal()}>Cancel</button>
-                        <button className="opo-modalbox__rightbutton" onClick={() => this.redirectToUrl(this.props.link)}>I want to leave</button>
+
+                    <div className="opo-modalbox__body">
+                        <div className="opo-modalbox__message">
+                            <p>Switching to Spanish will erase all the information you have previously entered.</p>
+                            <p>Are you sure you want to switch?</p>
+                        </div>
+                        <div className="opo-modalbox__buttons">
+                            <button className="opo-modalbox__rightbutton" onClick={() => this.redirectToUrl(this.props.link)}>Yes, start over in Spanish.</button>
+                            <button onClick={() => this.props.hideModal()}>No, proceed in English.</button>
+                        </div>
                     </div>
+
                 </div>
             </div>
 
-        );
+        ); // <!-- <div className="opo-modalbox__separator"></div> -->
     }
 }
 
