ecf4d0d7086e1006aed227bb72c7e0c10dae2e1c
Sergio Garcia <sergiogcx@gmail.com>
2019-03-21 23:07:02 -0500

    Updated Translations and update to usfs v0.0.15 (#23)

--- js/components/Confirmation.jsx
+++ js/components/Confirmation.jsx
@@ -11,7 +11,7 @@ class Confirmation extends React.Component {
         <SegmentedProgressBar total={2} current={2}/>
         <h2 style={{textAlign: "center"}}>We’re glad to hear you had a good experience!</h2>
         <hr/>
-        <p style={{fontSize: "1.4rem"}}>Your thank-you note will go to the Office of Police Oversight and the Austin Police Department’s Internal Affairs division, as well as to the officer’s chain of command. </p>
+        <p style={{fontSize: "1.4rem"}}>Your thank-you note will go to the Office of Police Oversight and the Austin Police Department’s Internal Affairs division, as well as to the officer’s chain of command.</p>
         <p style={{fontSize: "1.4rem"}}>If you provided your contact information, a staff person from the Office of Police Oversight will contact you within 2 to 4 business days, and you will receive an email with a copy of your thank-you note.</p>
       </div>
     );
