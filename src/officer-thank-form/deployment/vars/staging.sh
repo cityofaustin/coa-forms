e951fb84fc29d66fa3826365cadcfd143a9bf0bb
Niicck <niicck@users.noreply.github.com>
2019-04-17 15:37:35 -0500

    apply environment variables

---
+++ src/officer-thank-form/deployment/vars/staging.sh
@@ -0,0 +1,15 @@
+# AWS
+DEPLOYMENT_BUCKET=coa-forms-staging
+DEPLOYMENT_PATH_EN=police-thank
+DEPLOYMENT_PATH_ES=policia-agradezca
+
+# App
+NODE_ENV=development
+FORM_API_URL=https://6jm8rnjkxf.execute-api.us-east-1.amazonaws.com/staging
+
+# Structure
+FORM_DIR=officer-thank-form
+CHAPTERS_DIR=OPO
+
+# Debug
+# RUN_BUNDLE_ANALYZER=true
