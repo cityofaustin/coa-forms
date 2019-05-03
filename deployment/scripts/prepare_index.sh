c2eb8998f63b1246e73af6b5014782c4ed69f6b7
Sergio <sergiogcx@gmail.com>
2019-05-02 23:08:17 -0500

    Added FORM_PATH

--- deployment/scripts/prepare_index.sh
+++ deployment/scripts/prepare_index.sh
@@ -40,5 +40,5 @@ fi
 if [[ "${DEPLOY_ENV}" = "dev" ]]; then
     HOTJAR_CODE="<!-- Hotjar Tracking Code for https://opo-form.netlify.com/ or change url in their dashboard --> <script>(function(h, o, t, j, a, r){h.hj=h.hj || function(){(h.hj.q=h.hj.q || []).push(arguments);}; h._hjSettings={hjid: 1188991, hjsv: 6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script'); r.async=1; r.src=t + h._hjSettings.hjid + j + h._hjSettings.hjsv; a.appendChild(r);})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv='); </script>"
     HOTJAR_SNIPPET_MARKER="<!--HotJarSnippet-->"
-    sed -i'' -e "s%${HOTJAR_SNIPPET_MARKER}%${HOTJAR_CODE}%g" "$FORM_PATH/index.html"
+    sed -i'' -e "s%${HOTJAR_SNIPPET_MARKER}%${HOTJAR_CODE}%g" "${FORM_PATH}index.html"
 fi;
