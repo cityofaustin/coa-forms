96c7cd3cb0fc9e198017c777cf072786ea3d96b9
Eric Sherman <ericandrewsherman@gmail.com>
2019-07-12 14:27:06 -0500

    split about chapter into demographic info and follow up

--- shared/chapters/OPO/chapters/demographicInfoChapter.js
+++ shared/chapters/OPO/chapters/demographicInfoChapter.js
@@ -1,55 +1,41 @@
-import React from 'react';
-import { raceBlocks, genderBlocks } from '../../../schemaBlocks';
+import React from "react";
+import { raceBlocks, genderBlocks } from "../../../schemaBlocks";
 
-import PhoneNumberWidget from '@cityofaustin/us-forms-system/lib/js/widgets/PhoneNumberWidget';
-import PhoneNumberReviewWidget from '@cityofaustin/us-forms-system/lib/js/review/PhoneNumberWidget';
-import { phoneConfig } from '@cityofaustin/us-forms-system/lib/js/definitions/phone';
+import PhoneNumberWidget from "@cityofaustin/us-forms-system/lib/js/widgets/PhoneNumberWidget";
+import PhoneNumberReviewWidget from "@cityofaustin/us-forms-system/lib/js/review/PhoneNumberWidget";
+import { phoneConfig } from "@cityofaustin/us-forms-system/lib/js/definitions/phone";
 
 // we need to override this for labels, but need to make deep copys hence the JSON stuff
 let yourRace = JSON.parse(JSON.stringify(raceBlocks.ui));
 let yourGender = JSON.parse(JSON.stringify(genderBlocks.ui));
 
-yourRace.race['ui:title'] = 'Your race';
-yourGender.gender['ui:title'] = 'Your gender';
+yourRace.race["ui:title"] = "Your race";
+yourGender.gender["ui:title"] = "Your gender";
 
-const aboutYouChapter = {
-  title: 'Tell us about you (optional)',
+const demographicInfoChapter = {
+  title: "Tell us about you (optional)",
   pages: {
-    aboutYou: {
-      path: 'about-you',
-      title: 'Tell us about you',
+    demographicInfo: {
+      path: "about-you",
+      title: "Tell us about you",
       schema: {
-        type: 'object',
+        type: "object",
         properties: {
-          'view:infoObject': {
-            type: 'object',
-            properties: {},
+          "view:infoObject": {
+            type: "object",
+            properties: {}
           },
           ...genderBlocks.schema,
           ...raceBlocks.schema,
           zipCode: {
-            type: 'number'
-          },
-          'view:contactHeader': {
-            type: 'object',
-            properties: {},
-          },
-          willingToBeContacted: { type: 'boolean' },
-          'view:contactPreferences': {
-            type: 'object',
-            properties: {
-              yourName: { type: 'string' },
-              yourPhone: phoneConfig.schema(),
-              yourEmail: { type: 'string', format: 'email' },
-              needTranslator: { type: 'boolean' },
-            },
-          },
-        },
+            type: "number"
+          }
+        }
       },
       uiSchema: {
-        'ui:title': 'Tell us about you',
-        'view:infoObject': {
-          'ui:description': () => (
+        "ui:title": "Tell us about you",
+        "view:infoObject": {
+          "ui:description": () => (
             <div className="usa-alert  usa-alert-info">
               <div className="usa-alert-body">
                 <h2 className="usa-alert-heading">Demographic information</h2>
@@ -59,50 +45,19 @@ const aboutYouChapter = {
                 </p>
               </div>
             </div>
-          ),
+          )
         },
         ...yourGender,
         ...yourRace,
         zipCode: {
-          'ui:title': 'Your zip code',
-          'ui:options': {
-            hideOnReviewIfFalse: true,
-          },
-        },
-        'view:contactHeader': {
-          'ui:title': 'Contact preferences',
-        },
-
-        willingToBeContacted: {
-          'ui:title':
-            'I am willing to be contacted by the Office of Police Oversight in 2 to 4 business days.',
-          'ui:options': {
-            hideOnReviewIfFalse: true,
-          },
-        },
-        'view:contactPreferences': {
-          'ui:options': {
-            hideIf: formData => !formData.willingToBeContacted,
-          },
-          yourName: { 'ui:title': 'Your name' },
-          yourPhone: phoneConfig.uiSchema('Your phone number'),
-
-          yourEmail: {
-            'ui:title': 'Your email address',
-            'ui:widget': 'email',
-            'ui:description': '',
-            'ui:options': {
-              inputType: 'email',
-            },
-          },
-          needTranslator: {
-            'ui:title':
-              'I would like an interpreter for all my future interactions.',
-          },
-        },
-      },
-    },
-  },
+          "ui:title": "Your zip code",
+          "ui:options": {
+            hideOnReviewIfFalse: true
+          }
+        }
+      }
+    }
+  }
 };
 
-export default aboutYouChapter;
+export default demographicInfoChapter;
--- shared/chapters/OPO/chapters/followUpChapter.js
+++ shared/chapters/OPO/chapters/followUpChapter.js
@@ -1,77 +1,106 @@
-import React from 'react';
-import { raceBlocks, genderBlocks } from '../../../schemaBlocks';
+import React from "react";
+import { raceBlocks, genderBlocks } from "../../../schemaBlocks";
 
-import PhoneNumberWidget from '@cityofaustin/us-forms-system/lib/js/widgets/PhoneNumberWidget';
-import PhoneNumberReviewWidget from '@cityofaustin/us-forms-system/lib/js/review/PhoneNumberWidget';
-import { phoneConfig } from '@cityofaustin/us-forms-system/lib/js/definitions/phone';
+import PhoneNumberWidget from "@cityofaustin/us-forms-system/lib/js/widgets/PhoneNumberWidget";
+import PhoneNumberReviewWidget from "@cityofaustin/us-forms-system/lib/js/review/PhoneNumberWidget";
+import { phoneConfig } from "@cityofaustin/us-forms-system/lib/js/definitions/phone";
 
 // we need to override this for labels, but need to make deep copys hence the JSON stuff
 let yourRace = JSON.parse(JSON.stringify(raceBlocks.ui));
 let yourGender = JSON.parse(JSON.stringify(genderBlocks.ui));
+debugger;
+yourRace.race["ui:title"] = "Your race";
+yourGender.gender["ui:title"] = "Your gender";
 
-yourRace.race['ui:title'] = 'Your race';
-yourGender.gender['ui:title'] = 'Your gender';
-
-const aboutYouChapter = {
-  title: 'Follow up',
-  pages: {
-    aboutYou: {
-      path: 'follow-up',
-      title: 'Follow up',
-      schema: {
-        type: 'object',
-        properties: {
-          'view:contactHeader': {
-            type: 'object',
-            properties: {},
-          },
-          willingToBeContacted: { type: 'boolean' },
-          'view:contactPreferences': {
-            type: 'object',
-            properties: {
-              yourName: { type: 'string' },
-              yourPhone: phoneConfig.schema(),
-              yourEmail: { type: 'string', format: 'email' },
-              needTranslator: { type: 'boolean' },
-            },
-          },
-        },
-      },
-      uiSchema: {
-        'view:contactHeader': {
-          'ui:title': 'Contact preferences',
-        },
-
-        willingToBeContacted: {
-          'ui:title':
-            'I am willing to be contacted by the Office of Police Oversight in 2 to 4 business days.',
-          'ui:options': {
-            hideOnReviewIfFalse: true,
-          },
-        },
-        'view:contactPreferences': {
-          'ui:options': {
-            hideIf: formData => !formData.willingToBeContacted,
-          },
-          yourName: { 'ui:title': 'Your name' },
-          yourPhone: phoneConfig.uiSchema('Your phone number'),
-
-          yourEmail: {
-            'ui:title': 'Your email address',
-            'ui:widget': 'email',
-            'ui:description': '',
-            'ui:options': {
-              inputType: 'email',
-            },
-          },
-          needTranslator: {
-            'ui:title':
-              'I would like an interpreter for all my future interactions.',
-          },
-        },
-      },
-    },
+const followUpChoice = {
+  schema: {
+    remainAnonymous: {
+      type: "string",
+      enum: ["followUp", "remainAnonymous"],
+      enumNames: ["Follow up with me", "Remain anonymous"]
+    }
   },
+  ui: {
+    remainAnonymous: {
+      "ui:title":
+        "Do you want the Office of Police Oversight to follow up with you or would you like to remain anonymous?",
+      "ui:widget": "radio",
+      "ui:options": {
+        hideOnReviewIfFalse: false
+      }
+    }
+  }
 };
 
-export default aboutYouChapter;
+const followUpChapter = {
+  title: "Follow up",
+  pages: {
+    followUp: {
+      path: "follow-up",
+      title: "Follow up",
+      schema: {
+        type: "object",
+        properties: {
+          "view:contactHeader": {
+            type: "object",
+            properties: {}
+          },
+          ...followUpChoice.schema,
+          "view:contactPreferences": {
+            type: "object",
+            properties: {
+              yourName: { type: "string" },
+              yourPhone: phoneConfig.schema(),
+              yourEmail: { type: "string", format: "email" },
+              needTranslator: { type: "boolean" }
+            }
+          },
+          "view:infoObject": {
+            type: "object",
+            properties: {}
+          }
+        }
+      },
+      uiSchema: {
+        "ui:title": "Follow up",
+        ...followUpChoice.ui,
+        "view:contactPreferences": {
+          "ui:options": {
+            expandUnder: "remainAnonymous",
+            expandUnderCondition: "followUp"
+          },
+          yourName: { "ui:title": "Your name" },
+          yourPhone: phoneConfig.uiSchema("Your phone number"),
+
+          yourEmail: {
+            "ui:title": "Your email address",
+            "ui:widget": "email",
+            "ui:description": "",
+            "ui:options": {
+              inputType: "email"
+            }
+          },
+          needTranslator: {
+            "ui:title":
+              "I would like an interpreter for all my future interactions."
+          }
+        },
+        "view:infoObject": {
+          "ui:description": () => (
+            <p className="usa-font-lead">
+              Your complaint will be reviewed even if you remain anonymous.
+              However, we will not be able to contact you with any follow-up
+              questions or give you any information about the case.
+            </p>
+          ),
+          "ui:options": {
+            expandUnder: "remainAnonymous",
+            expandUnderCondition: "remainAnonymous"
+          }
+        }
+      }
+    }
+  }
+};
+
+export default followUpChapter;
