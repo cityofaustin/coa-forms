aa1a433c1918da8885b2a47f114ccec3e4f7ed17
Eric Sherman <ericandrewsherman@gmail.com>
2019-08-13 14:36:58 -0500

    link to backend

--- forms/day-labor/deployment/vars/dev.sh
+++ forms/day-labor/deployment/vars/dev.sh
@@ -15,5 +15,6 @@ FORM_DIR=day-labor
 # RUN_BUNDLE_ANALYZER=true
 
 if [ "$CIRCLE_BRANCH" == "2626-day-labor-content" ]; then
-  FORM_API_URL=https://dmno3z98d6.execute-api.us-east-1.amazonaws.com/pr_2626_day_labor_content
+  FORM_API_URL=https://urz68t6uk8.execute-api.us-east-1.amazonaws.com/pr_2626_day_labor_email
 fi
+2626-day-labor-email
