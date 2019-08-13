13be6e395d6e73a3d83ec10bf23ae02d35ed8949
Eric Sherman <ericandrewsherman@gmail.com>
2019-08-13 13:00:47 -0500

    comment out learn more til we have an alpha link

--- forms/day-labor/src/js/components/Confirmation.jsx
+++ forms/day-labor/src/js/components/Confirmation.jsx
@@ -197,7 +197,7 @@ class Confirmation extends React.Component {
             <h2>We have received your request</h2>
             <p>Thank you for submitting your request to hire a day laborer! We’ll contact you within 24 hours to confirm. You can also call us at (512) 972-4100.</p>
             <p>If you provided your email, you’ll receive a copy of your request.</p>
-            <p><a href="https://docs.google.com/document/d/16siLTqjO5klJpgUt5P4UABnFR2k4ed6NUtyMqkm61Vs">Learn more about hiring a day laborer through the First Workers Day Labor Center.</a></p>
+            {/*<p><a href="https://docs.google.com/document/d/16siLTqjO5klJpgUt5P4UABnFR2k4ed6NUtyMqkm61Vs">Learn more about hiring a day laborer through the First Workers Day Labor Center.</a></p>*/}
           </div>
         );
   }
