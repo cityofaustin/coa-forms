8d33f7dce3990424f9de0ac231309dc2c090adbc
Sergio Garcia <sergiogcx@gmail.com>
2021-10-19 15:53:46 -0500

    Update all staging endpoints

--- forms/officer-complaint-form/deployment/vars/staging.sh
+++ forms/officer-complaint-form/deployment/vars/staging.sh
@@ -5,7 +5,7 @@ DEPLOYMENT_PATH_ES=policia-queja
 
 # App
 NODE_ENV=development
-FORM_API_URL=https://ep5o74k5b5.execute-api.us-east-1.amazonaws.com/staging
+FORM_API_URL=https://7p7ja2j59b.execute-api.us-west-2.amazonaws.com/staging
 
 # Structure
 FORM_DIR=officer-complaint-form
